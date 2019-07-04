608d7a63ed52a923ce0511373d892f3ea321950d
daniraVazquezC <danira.vazquez2811@gmail.com>
2019-07-04 16:39:48 -0500

    migration and model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :author, presence:true
-  validates :resume, presence: true
+  validates :resume, presence: true , length:{ minimum: 10}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_04_204936) do
+ActiveRecord::Schema.define(version: 2019_07_04_211719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "resume"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

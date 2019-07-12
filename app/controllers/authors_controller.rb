0691db397e98499a5d33265a240619e7302e0a08
daniraVazquezC <danira.vazquez2811@gmail.com>
2019-07-12 15:55:23 -0500

    add index method in authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,18 @@
 class AuthorsController < ApplicationController
 
+  def index
+    if params[:author] != '' 
+      @author = Author.where("lower(name) like ?","%#{params[:author].downcase}%")
+      render json: @author
+    else
+      @authors = Author.all
+      render json: @authors
+    end
+  end
+
+
   def show
-    @author = Author.find(params[:id])
-    render json: @author
+      @author = Author.find(params[:id])
+      render json: @author    
   end
 end
